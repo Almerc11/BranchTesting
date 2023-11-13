9eb1a8c530f31a4e8324cf6fdc5865585d036fc6
Almer Toro Coskun <alco0001@stud.kea.dk>
2023-11-13 09:41:44 +0100

    Tilføjet ny eat branch

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -2,6 +2,10 @@ public class Dog extends Animal {
 
     public void makeSound() {
         System.out.println("vov vov");
+}
+    @Override
+    public boolean eat(String foodType) {
+        return true;
 
     }
 }
